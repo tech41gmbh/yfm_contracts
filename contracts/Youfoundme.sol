e7f89cd6ad1e22033d84869872880a7624f62979
tech41-de <mathias.dietrich@tech41.de>
2021-12-13 09:29:32 +0100

    changed to standard token format

--- contracts/Youfoundme.sol
+++ contracts/Youfoundme.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Youfoundme is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     constructor() ERC721("Youfoundme", "YFM") {}
 
-    function mintNFT(address recipient, uint256 newItemId, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256){
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
