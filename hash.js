a857923a66d343867b5d3084aa3edef8025c000b
tech41-de <mathias.dietrich@tech41.de>
2021-12-13 16:44:58 +0100

    changed token hash

--- hash.js
+++ hash.js
@@ -5,8 +5,8 @@ const { getHasher, OutputType, HashType } = require('bigint-hash');
 
 var did = "did:yfm:eip-155:43113:345879B60BF5ccDDd06BC91E49A6eBc4e93CfDAa";
 
-
-
-var decimal = getHasher(HashType.SHA3_256).update(Buffer.from(did)).digest(OutputType.BigInt);
+var decimal = getHasher(HashType.SHA3_224).update(Buffer.from(did)).digest(OutputType.BigInt);
 
 console.log(decimal)
+
+
