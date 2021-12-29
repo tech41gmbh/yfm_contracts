693ef4b69caceeba5457f66761723eacb7e9b773
tech41-de <mathias.dietrich@tech41.de>
2021-12-29 13:04:48 +0100

    changed tokenid generation

--- hash.js
+++ hash.js
@@ -1,12 +1,19 @@
 
 const { getHasher, OutputType, HashType } = require('bigint-hash');
-
+const Web3 = require('web3');
 
 // DID is lowercase !
 
-var did = "did:yfm:eip-155:43113:0x58d22f24cd7fda155f9f7eefe9b32add46388ab9";
 
-var decimal = "" + getHasher(HashType.SHA3_224).update(Buffer.from(did.toLowerCase())).digest(OutputType.BigInt);
+function TokenIdFromDid(did) {
+
+  const web3 = new Web3("ws://localhost:8545");
+  return web3.utils.hexToNumberString(Web3.utils.soliditySha3(did));
+}
+
+var did = "did:yfm:0x345879B60BF5ccDDd06BC91E49A6eBc4e93CfDAa";
+
+var decimal = TokenIdFromDid(did);
 
 console.log(decimal)
 
